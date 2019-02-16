27fead38e5de6be4c1dedbecbb99d2c0da00e1a9
Amer <toukan.amer@gmail.com>
2019-02-16 08:56:09 -0500

    nav ready

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Image } from 'react-native';
 import {createBottomTabNavigator} from 'react-navigation';
-
+import Icon from 'react-native-vector-icons/Ionicons'
 import Explore from './screens/Explore';
 import Inbox from './screens/Inbox';
 import Saved from './screens/Saved';
@@ -22,16 +22,43 @@ class App extends React.Component {
 
 export default createBottomTabNavigator({
   Explore: { 
-    screen: Explore
+    screen: Explore,
+    navigationOptions: {
+      tabBarLabel: 'EXPLORE', 
+      tabBarIcon: ({tintColor}) => (
+        <Icon name = 'ios-search' color = {tintColor} size ={24}/> 
+      )
+    }
   }, 
   Saved: { 
-    screen: Saved
+    screen: Saved,
+    navigationOptions: {
+      tabBarLabel: 'SAVED', 
+      tabBarIcon: ({tintColor}) => (
+        <Icon name = 'ios-heart' color = {tintColor} size ={24}/> 
+      )
+    }
   }, 
   Trips: {
-    screen: Trips
+    screen: Trips,
+    navigationOptions: {
+      tabBarLabel: 'TRIPS', 
+      tabBarIcon: ({tintColor}) => (
+       <Image source = { require ('./assets/images/airbnb.png') } 
+       style = {{ 
+         height: 24, width: 24, tintColor: tintColor}}
+       />
+      )
+    }
   },
   Inbox: {
-    screen: Inbox
+    screen: Inbox, 
+    navigationOptions: {
+      tabBarLabel: 'INBOX', 
+      tabBarIcon: ({tintColor}) => (
+        <Icon name = 'ios-chatboxes' color = {tintColor} size ={24}/> 
+      )
+    }
   }
 })
 

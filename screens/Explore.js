ad89b7fe57ee3dce8e766251cd54bace9c5c929d
Amer <toukan.amer@gmail.com>
2019-02-16 15:21:46 -0500

    home page almost ready

--- screens/Explore.js
+++ screens/Explore.js
@@ -7,11 +7,18 @@ import {
     SafeAreaView,
     TextInput,
     Platform, 
-    StatusBar
+    StatusBar,
+    ScrollView,
+    Image, 
+    Dimensions
 } from 'react-native'; 
 
-import Icon from 'react-native-vector-icons/Ionicons'
+import Icon from 'react-native-vector-icons/Ionicons';
+import Category from './components/Explore/category';
 import { Status } from 'expo-background-fetch';
+import { white } from 'ansi-colors';
+
+const {height, width} = Dimensions.get('window')
 class Explore extends Component { 
     componentWillMount(){
         this.startHeaderHeight = 80
@@ -56,7 +63,7 @@ class Explore extends Component {
     
     <TextInput
     underlineColorAndroid = "transparent"
-        placeholder = "Try Toronto"
+        placeholder = "Explore Toronto"
         placeholderTextColor = 'grey'
         style = 
             {{
@@ -70,6 +77,86 @@ class Explore extends Component {
     </Icon>
     </View>
     </View>
+    <ScrollView
+    scrollEventThrottle = {16}
+    >
+    <View style = {{
+        flex: 1,
+        backgroundColor: 'white', 
+        paddingTop: 20,
+    }}>
+    <Text
+    style = {{
+        fontSize: 24, 
+        fontWeight: '700',
+        paddingHorizontal: 20
+    }}>
+    What can we help you find, Amer?
+    </Text>
+    <View style = {{
+        height : 130,
+        marginTop: 20
+    }}> 
+    <ScrollView
+    horizontal = {true}
+    showsHorizontalScrollIndicator = {false}
+    >
+    
+    <Category 
+    imageUri = {require ('../assets/images/home.jpg')}
+    name = 'Home'
+    />
+
+    <Category 
+    imageUri = {require ('../assets/images/restaurant.jpg')}
+    name = 'Restaurant'
+    />
+
+    <Category 
+    imageUri = {require ('../assets/images/experiences.jpg')}
+    name = 'Experiences'
+    />
+    </ScrollView>
+    </View>
+
+
+    <View style = {{
+        marginTop: 40, 
+        paddingHorizontal: 20
+    }}>
+    <Text style = {{
+        fontSize: 24, fontWeight: '700'
+    }}> 
+    Introducing Airbnb Plus
+    </Text>
+
+    <Text  style = {{
+        fontWeight: '100', 
+        marginTop: 10
+    }}>
+    A new selection of homes verified for quality and comfort
+    </Text>
+
+    <View style = {{
+        width: width-40, 
+        height: 200,
+        marginTop: 20,
+    }}>
+    <Image style = {{
+        flex: 1, 
+        height: null, 
+        width: null,
+        resizeMode: 'cover', 
+        borderRadius: 5, 
+        borderWidth:1, 
+        borderColor: '#dddddd'
+    }}
+    source = {require ('../assets/images/home.jpg')}
+    />
+    </View>
+    </View>
+    </View>
+    </ScrollView>
     </View>
     </SafeAreaView>
     )}};
